539f72b0c519613e8da3dc8f3a421c034bfe9719
gr8mao <gr8mao@gmail.com>
2016-04-21 17:37:43 +0300

    Made some work. Dont even remember what I have done

---
+++ test1.py
@@ -0,0 +1,83 @@
+
+from collections import OrderedDict
+import os
+
+
+def read_table(name_of_table):
+    name_of_table += '.txt'
+    try:
+        file = open(name_of_table, 'r')
+    except IOError:
+        print('Table is not exist')
+        return
+    data = []
+    table = []
+    for str in file:
+        str = ''.join(str.split())
+        temp = str.split('|')
+        data.append(temp)
+    if len(data) < 2:
+        temp = dict(zip(data[0], ['']*len(temp)))
+        table.append(temp)
+    for elem in data[1:]:
+        temp = OrderedDict(zip(data[0], elem))
+        table.append(temp)
+    return table
+
+
+def save_table(table, name_of_table):
+    name_of_table += '.txt'
+    data = []
+    for row in table:
+        headers = list(row.keys())
+        line = []
+        for header in headers:
+            elem = row[header]
+            line.append(elem)
+        data.append(line)
+    data.insert(0, headers)
+    file = open(name_of_table, 'w')
+    for line in data:
+        stri = ''
+        for elem in line:
+            stri += str(elem).ljust(20, " ") + '|'
+        file.write(stri.rstrip("|") + '\n')
+
+
+def show(name_of_table):
+    table = read_table(name_of_table)
+    data = []
+    for row in table:
+        headers = list(row.keys())
+        line = []
+        for header in headers:
+            elem = row[header]
+            line.append(elem)
+        data.append(line)
+    data.insert(0, headers)
+    for line in data:
+        stri = ''
+        for elem in line:
+            stri += str(elem).ljust(20, " ") + '|'
+        print(stri.rstrip("|"))
+        
+        
+def get_headers(headers):
+    types = []
+    names = []
+    for header in headers:
+        header = header.split('>')
+        header[0] = header[0][header[0].index('<') + 1:]
+        types.append(header[0])
+        names.append(header[1])
+    return OrderedDict(zip(names, types))
+    
+
+def get_values_from_dict(table, keys):
+    values = []
+    for elem in table:
+        for header in keys:
+            values.append(elem[header])
+    return values
+if __name__ == '__main__': 
+	show('table2')
--- translator.py
+++ translator.py
@@ -1,13 +1,12 @@
 from collections import OrderedDict
 
-commands = ['show', 'mktable', 'addnote', 'delnote', 'deltable', 'filter', 'sort', 'jointab']
-mainPrinted = False
-GenPrinted = False
-table = 'table.txt'
-main = ''
+commands = ['show', 'mktable', 'addnote', 'delnote', 'deltable', 'filter', 'sort', 'jointab']  # Возможные функции
+mainPrinted = False  # Флаг о записи вызова функции main
+GenPrinted = False  # Флаг о записи общего модуля
+main = ''  # Хранение вызовов функций в транслированной программе
 
 
-def errors_exec(code, *args):
+def errors_exec(code, *args):  # Модуль ошибок. Выводит текст ошибки на экран в зависимости от кода ошибки и завершает программу
     print({
               # Общие ошибки
               '0101': 'Не найдено ключевое слово',
@@ -39,11 +38,11 @@ def errors_exec(code, *args):
     exit(int(code))
 
 
-def translator(action, *args):
-    global mainPrinted, err, main, GenPrinted
+def translator(action, *args):  # Функция транслирования файла. Входные данные: код программы, массив аргументов
+    global mainPrinted, err, main, GenPrinted 
 
-    if not GenPrinted:
-        file = open('new_funcs', 'r')
+    if not GenPrinted:  # Запись общего модуля
+        file = open('functions', 'r')
         if not file:
             raise IOError
         line = file.read()
@@ -52,32 +51,32 @@ def translator(action, *args):
         rez.write(genral)
         GenPrinted = True
 
-    if action == 'mktable':  # Функция создания таблицы ready
+    if action == 'mktable':  # Запись модуля создания таблицы
         file = open('new_funcs', 'r')
         if not file:
             raise IOError
         line = file.read()
-        function = find_between(line, '@' + action, '@SEP')
+        function = find_between(line, '@' + action, '@SEP')  # Считываем код функции-эквивалента из базы
         rez = open('test1.py', 'a')
-        temp1 = open('test1.py', 'r')
-        temp = temp1.read()
-        if action not in temp:
+        res_file = open('test1.py', 'r')
+        temp = res_file.read()
+        if action not in temp:  # Записана ли функция в выходной файл?
             rez.write(function)
         if not mainPrinted:
             main += "if __name__ == '__main__': \n"
             mainPrinted = True
-        main += "\t" + action + "('" + args[0] + "'," + str(args[1]) + ")\n"
-    # Конец mktable
+        main += "\t" + action + "('" + args[0] + "'," + str(args[1]) + ")\n"  # Формирование вызова функции mktable
+    # Конец модуля создания таблицы
 
-    if action == 'addnote':  # Функция создания записи в таблице ready
+    if action == 'addnote':  # Запись модуля создания записи в таблице
         file = open('new_funcs', 'r')
         if not file:
             raise IOError
         line = file.read()
         function = find_between(line, '@' + action, '@SEP')
         rez = open('test1.py', 'a')
-        temp1 = open('test1.py', 'r')
-        temp = temp1.read()
+        res_file = open('test1.py', 'r')
+        temp = res_file.read()
         if action not in temp:
             rez.write(function)
         if not mainPrinted:
@@ -86,15 +85,15 @@ def translator(action, *args):
         main += "\t" + action + "('" + args[0] + "'," + str(args[1]) + "," + str(args[2]) + ")\n"
     # Конец addnote
 
-    if action == 'deltable':  # Команда удаление таблицы ready
+    if action == 'deltable':  # Запись модуля удаления таблицы
         file = open('new_funcs', 'r')
         if not file:
             raise IOError
         line = file.read()
         function = find_between(line, '@' + action, '@SEP')
         rez = open('test1.py', 'a')
-        temp1 = open('test1.py', 'r')
-        temp = temp1.read()
+        res_file = open('test1.py', 'r')
+        temp = res_file.read()
         if action not in temp:
             rez.write(function)
         if not mainPrinted:
@@ -103,15 +102,15 @@ def translator(action, *args):
         main += "\t" + action + "('" + args[0] + "')\n"
     # Конец deltable
 
-    if action == 'sort':  # Команда сортировки таблицы *переделать*
+    if action == 'sort':  # Запись модуля сортировки таблицы
         file = open('new_funcs', 'r')
         if not file:
             raise IOError
         line = file.read()
         function = find_between(line, '@' + action, '@SEP')
         rez = open('test1.py', 'a')
-        temp1 = open('test1.py', 'r')
-        temp = temp1.read()
+        res_file = open('test1.py', 'r')
+        temp = res_file.read()
         if action not in temp:
             rez.write(function.replace('@key', str(args[1])))
         if not mainPrinted:
@@ -120,15 +119,15 @@ def translator(action, *args):
         main += "\t" + action + "('" + args[0] + "')\n"
     # Конец sort
 
-    if action == 'jointab':  # Команда объедения таблиц
+    if action == 'jointab':  # Запись модуля объедения таблиц
         file = open('new_funcs', 'r')
         if not file:
             raise IOError
         line = file.read()
         function = find_between(line, '@' + action, '@SEP')
         rez = open('test1.py', 'a')
-        temp1 = open('test1.py', 'r')
-        temp = temp1.read()
+        res_file = open('test1.py', 'r')
+        temp = res_file.read()
         if action not in temp:
             rez.write(function)
         if not mainPrinted:
@@ -137,22 +136,22 @@ def translator(action, *args):
         main += "\t" + action + "('" + args[0] + "','" + args[1] + "')\n"
     # Конец jointab
 
-    if action == 'show':  # Команда показа таблицы
+    if action == 'show':  # Запись модуля показа таблицы
         if not mainPrinted:
             main += "if __name__ == '__main__': \n"
             mainPrinted = True
         main += "\t" + action + "('" + args[0] + "')\n"
     # Конец show
 
-    if action == 'delnote':  # Команда удаление записи
+    if action == 'delnote':  # Запись модуля удаления записи
         file = open('new_funcs', 'r')
         if not file:
             raise IOError
         line = file.read()
         function = find_between(line, '@' + action + '_' + args[3], '@SEP')
         rez = open('test1.py', 'a')
-        temp1 = open('test1.py', 'r')
-        temp = temp1.read()
+        res_file = open('test1.py', 'r')
+        temp = res_file.read()
         if action not in temp:
             function = function.replace('@key', str(args[1]))
             function = function.replace('@condition', str(args[2]))
@@ -163,15 +162,15 @@ def translator(action, *args):
         main += "\t" + action + "('" + args[0] + "')\n"
     # Конец delnote
 
-    if action == 'filter':  # Команда филтер таблицы
+    if action == 'filter':  # Запись модуля филтер таблицы
         file = open('new_funcs', 'r')
         if not file:
             raise IOError
         line = file.read()
         function = find_between(line, '@' + action + '_' + args[3], '@SEP')
         rez = open('test1.py', 'a')
-        temp1 = open('test1.py', 'r')
-        temp = temp1.read()
+        res_file = open('test1.py', 'r')
+        temp = res_file.read()
         if action not in temp:
             function = function.replace('@key', str(args[1]))
             function = function.replace('@condition', str(args[2]))
@@ -183,29 +182,28 @@ def translator(action, *args):
         # Конец filter
 
 
-def reader(command, line_number):
-    command = ' '.join(command.split())
-    command = command.split(' ')
-    action = command[0]
-    if action not in commands:
-        errors_exec('0101', action, line_number)
-    check_syntax(command, action, line_number)
-
-def get_headers(headers):
+def get_headers(headers):  # Функция создания создания словаря (<Поле в таблице>: <тип данных в поле>)
     types = []
     names = []
     headers = ''.join(headers.split())
     headers = headers.split('|')
     for header in headers:
         header = header.split('>')
-        header[0] = header[0][header[0].index('<') + 1:]
+        header[0] = header[0][header[0].index('<') + 1:]  # Выделение спецификации типа из строки
         types.append(header[0])
         names.append(header[1])
-    return OrderedDict(zip(names, types))
+    return OrderedDict(zip(names, types))  # Возравщает упорядоченный словарь (<Поле в таблице>: <тип данных в поле>)
 
 
-def check_syntax(command, action, line_number):
-    if action == 'mktable':  # Функция создания таблицы
+def check_syntax(command, line_number):  # Функция синатксического и лексического разбора команды
+    command = ' '.join(command.split())
+    command = command.split(' ')
+    action = command[0]
+
+    if action not in commands:  # Проверка ключего слова команды
+        errors_exec('0101', action, line_number)
+
+    if action == 'mktable':  # Проверка синтаксиса команды создания таблицы
         if command[2] != 'inwhich':
             errors_exec('0201', action,line_number)
         else:
@@ -217,123 +215,149 @@ def check_syntax(command, action, line_number):
             args = temp.split(',')
             n = 0
             for arg in args:
-                if arg.find('<str>') != -1 or arg.find('<int>') != -1 or arg.find('<float>') != -1:
+                if arg.find('<str>') != -1 or arg.find('<int>') != -1 or arg.find('<float>') != -1:  # Проверка указания спецификации типов
                     n += 1
                     continue
                 else:
                     errors_exec('0202', arg, line_number)
-            if n != len(list(get_headers(temp.replace(',','|')).keys())):
+            if n != len(list(get_headers(temp.replace(',','|')).keys())):  # Проверка на указание одинаковых имен полей
                 errors_exec('0203','',line_number)
-            translator(action, name_of_table, args)
+
+            translator(action, name_of_table, args)  # Передача аргументов команды в транслятор
     # Конец mktable
 
-    if action == 'addnote':  # Функция создания записи в таблице
+    if action == 'addnote':  # Проверка синтаксиса команды создания записи в таблице
         command.pop(0)
+
         try:
             keys = ''.join(command[:command.index('values')])
             keys.replace(' ', '')
         except:
             errors_exec('0301', "values", line_number)
+
         command = command[command.index('values') + 1:]
+
         try:
             values = ''.join(command[:command.index('in')])
             values.replace(' ', '')
         except:
             errors_exec('0301', "in", line_number)
+
         keys = keys.split(',')
         values = values.split(',')
+
         try:
             name_of_table = command[command.index('in') + 1:].pop(0)
         except:
             errors_exec('0302', name_of_table, line_number)
+
         for value in values:
             if len(value) > 20:
                 errors_exec('0303', value, line_number)
-        translator(action, name_of_table, keys, values)
+
+        translator(action, name_of_table, keys, values) # Передача аргументов команды в транслятор
     # Конец addnote в check_syntax
 
-    if action == 'deltable':  # Команда удаление таблицы
+    if action == 'deltable':  # Проверка синтаксиса команды удаление таблицы
         command.pop(0)
+
         if len(command) > 1 or len(command) == 0:
             errors_exec('0401', command, line_number)
+
         name_of_table = command.pop()
         translator(action, name_of_table)
     # Конец deltable
 
-    if action == 'sort':  # Команда сортировки таблицы
+    if action == 'sort':  # Проверка синтаксиса команды сортировки таблицы
         command.pop(0)
         name_of_table = command.pop(0)
+
         try:
             key = command[command.index('by') + 1:]
             if len(key) > 2:
                 errors_exec('0502', 1, line_number)
         except:
             errors_exec('0501', "by", line_number)
+
         key = key.pop(0)
-        # try:
-        #     mode = command[command.index(key) + 1:].pop(0)
-        # except:
-        #     pass
         translator(action, name_of_table, key)
     # Конец sort
 
-    if action == 'jointab':  # Команда объедения таблиц
+    if action == 'jointab':  # Проверка синтаксиса команды объедения таблиц
         command.pop(0)
+
         if command[1] != 'with':
             errors_exec('0601', 'with', line_number)
+
         if len(command) > 3:
             errors_exec('0602', '', line_number)
+
         name_of_table1 = command.pop(0)
         command.pop(0)
         name_of_table2 = command.pop(0)
-        translator(action, name_of_table1, name_of_table2)
+
+        translator(action, name_of_table1, name_of_table2) # Передача аргументов команды в транслятор
     # Конец jointab
 
-    if action == 'show':  # Команда показа таблицы
+    if action == 'show':  # Проверка синтаксиса команды показа таблицы
         command.pop(0)
+
         if len(command) > 1:
             errors_exec('0701', command, line_number)
+
         name_of_table = command.pop()
-        translator(action, name_of_table)
+
+        translator(action, name_of_table) # Передача аргументов команды в транслятор
     # Конец show
 
-    if action == 'delnote':  # Команда удаление записи
+    if action == 'delnote':  # Проверка синтаксиса команды удаление записи
         command.pop(0)
+
         if command[1] != 'where':
             errors_exec('0801', 'where', line_number)
+
         name_of_table = command.pop(0)
         condition = command[command.index('where') + 1:]
         condition = ' '.join(condition)
+
         if condition.find('=') != -1:
             condition = condition.replace('=','==')
+
         temp = condition.split(' ')
+
         if len(temp) < 3:
             errors_exec("0802", condition, line_number)
+
         key = temp.pop(0)
         print(condition)
         value = temp.pop()
         type = check_type(value)
-        translator(action, name_of_table, key, condition, type)
+        translator(action, name_of_table, key, condition, type) # Передача аргументов команды в транслятор
     # Конец delnote
 
-    if action == 'filter':  # Команда филтер таблицы
+    if action == 'filter':  # Проверка синтаксиса команды филтер таблицы
         command.pop(0)
+
         if command[1] != 'by':
             errors_exec('0901', 'by', line_number)
+
         name_of_table = command.pop(0)
         condition = command[command.index('by') + 1:]
         condition = ' '.join(condition)
         temp = condition.split(' ')
+
         if len(temp) < 3:
             errors_exec("0902", condition, line_number)
+
         key = temp.pop(0)
         value = temp.pop()
         type = check_type(value)
-        translator(action, name_of_table, key, condition, type)
+
+        translator(action, name_of_table, key, condition, type) # Передача аргументов команды в транслятор
         # Конец filter
 
 
-def check_type(value):
+def check_type(value):  # Функция проверки типа. Возвращает наименование типа
     if value.isdigit():
         return 'int'
     elif value.isdecimal():
@@ -342,7 +366,7 @@ def check_type(value):
         return 'str'
 
 
-def find_between(s, first, last):
+def find_between(s, first, last):  # Функция считывания по разделителям
     try:
         start = s.index(first) + len(first)
         end = s.index(last, start)
@@ -351,12 +375,12 @@ def find_between(s, first, last):
         return ""
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # Функция main
     file = open('test1.py', 'w')
     file.close()
     enter_file = open('to_compile.txt', 'r')
     for index, line in enumerate(enter_file):
-        reader(line, index + 1)
+        check_syntax(line, index + 1)
     file = open('test1.py', 'a')
     file.write(main)
-    print('Файл успешно транслирован')
+    print('Файл успешно транслирован')  # Вывод сообщение об успешной транляции
